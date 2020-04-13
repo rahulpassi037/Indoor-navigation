6a7a33153c694bcf0c80d5904992b48fd1ddbbbc
Rahul <rahulpassi037@gmail.com>
2020-04-13 12:06:25 +0530

    Camera functionality

--- app/src/main/java/com/example/nsutcampusnavigator/MainActivity.java
+++ app/src/main/java/com/example/nsutcampusnavigator/MainActivity.java
@@ -5,25 +5,17 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int MY_PERMISSIONS_REQUEST = 123;
-
-//    protected boolean allPermissionsGranted(){
-//        return ((ContextCompat.checkSelfPermission(this, Manifest.permission.BODY_SENSORS)
-//                == PackageManager.PERMISSION_GRANTED) &&
-//                (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-//                        == PackageManager.PERMISSION_GRANTED) &&
-//                (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-//                        == PackageManager.PERMISSION_GRANTED) &&
-//                (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
-//                        == PackageManager.PERMISSION_GRANTED));
-//    }
 
     protected void checkAllPermissions(){
         ArrayList<String> permissions = new ArrayList<String>();
@@ -56,15 +48,26 @@ public class MainActivity extends AppCompatActivity {
                     MY_PERMISSIONS_REQUEST);
     }
 
-//    protected void ShowerrorandExit(){
-//          MainActivity.this.finish();
-//          System.exit(0);
-//    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         checkAllPermissions();
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.button4);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Mapping.class));
+            }
+        });
+
+        Button btn1 = (Button)findViewById(R.id.button3);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Navigation.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/nsutcampusnavigator/Mapping.java
@@ -0,0 +1,90 @@
+package com.example.nsutcampusnavigator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Mapping extends AppCompatActivity {
+
+    String currentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = "Rahul";
+        //timeStamp = new SimpleDateFormat("yyyyMMddRahul").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.nsutcampusnavigator.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+//    private void galleryAddPic() {
+//        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_STARTED);
+//        File f = new File(currentPhotoPath);
+//        Uri contentUri = Uri.fromFile(f);
+//        mediaScanIntent.setData(contentUri);
+//        this.sendBroadcast(mediaScanIntent);
+//    }
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+//            Bundle extras = data.getExtras();
+//            Bitmap imageBitmap = (Bitmap) extras.get("data");
+//            ImageView imageView = findViewById(R.id.imageview1);
+//
+//            imageView.setImageBitmap(imageBitmap);
+//        }
+//    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mapping);
+        dispatchTakePictureIntent();
+    }
+}
